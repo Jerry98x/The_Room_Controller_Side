0e033ceaef828ff6ae209122e3746bd7785237f6
Andrea Gerosa <61738886+Jerry98x@users.noreply.github.com>
2024-05-23 09:58:53 +0200

    Added audio manager + experiments on simple particle system VFXs

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,21 @@
+// ReSharper disable InconsistentNaming
+public class Constants {
+    
+    #region Layers
+    public const string NETO_LAYER = "NetoLayer";
+    public const string SAURON_LAYER = "SauronLayer";
+    public const string NETO_LAYER_WALL = "NetoLayerWall";
+    public const string SAURON_LAYER_WALL = "SauronLayerWall";
+    #endregion
+    
+    #region Mixer
+    public const string MIXERGROUP_MASTER = "Master";
+    public const string MIXERGROUP_MUSIC = "Master/Music";
+    public const string MIXERGROUP_SFX = "Master/SFX";
+    public const string MIXERPARAM_MASTERVOLUME = "MasterVolume";
+    public const string MIXERPARAM_MUSICVOLUME = "MusicVolume";
+    public const string MIXERPARAM_SFXVOLUME = "SFXVolume";
+    #endregion
+
+
+}
---
+++ Assets/Scripts/Utils/Sound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    // --- Public members
+    public string name;
+    public AudioClip clip;
+    [Range (0f,1f)] public float volume = 0.5f;
+    [Range(0.1f, 3f)] public float pitch = 1f;
+    public bool loop;
+    [Range(0, 256)] public int priority = 128;
+    [Range(-1f, 1f)] public float panStereo = 0f;
+    [Range (0f,1f)] public float spatialBlend = 0f;
+    // AudioSource set in the AudioManager
+    [HideInInspector] public AudioSource source;
+     
+
+}
